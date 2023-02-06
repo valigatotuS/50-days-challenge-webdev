61027f61568ff04f8a4f892574ee00c95a6e4c51
valigatotuS <vaquevy.pro@gmail.com>
2023-02-06 19:45:07 +0100

    add day1 project

---
+++ #day_1/script.js
@@ -0,0 +1,20 @@
+// Get all cards
+// const cards = document.querySelectorAll('.card');
+
+// // Add click event listener to all cards
+// cards.forEach(card => {
+//   card.addEventListener('click', function() {
+//     // Remove active class from all cards
+//     cards.forEach(c => {
+//       c.classList.remove('active');
+//     });
+//     // Add active class to the selected card
+//     this.classList.add('active');
+//   });
+// });
+
+// $(".card").hover(function() {
+//     $(this).css("transform", "scale(1.2)");
+//   }, function() {
+//     $(this).css("transform", "scale(1)");
+//   });
